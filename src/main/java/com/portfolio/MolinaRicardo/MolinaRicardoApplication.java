8d6499ccca87f76fd95c9119be4714c46d1e027d
Ricardo Molina <89365330+molinaricardo@users.noreply.github.com>
2022-09-28 03:35:06 -0300

    https://start.spring.io/
    Se crea la estructura del proyecto.

---
+++ src/main/java/com/portfolio/MolinaRicardo/MolinaRicardoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.MolinaRicardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolinaRicardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolinaRicardoApplication.class, args);
+	}
+
+}
